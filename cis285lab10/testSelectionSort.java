d03f632ce073c22752926ccbe33f26b39f40c41d
StukC <stukc@umich.edu>
2022-04-13 19:34:15 -0400

    positive and negative

--- cis285lab10/testSelectionSort.java
+++ cis285lab10/testSelectionSort.java
@@ -1,24 +1,21 @@
-
 /*
  * testSelectionSort.java
  *
 * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-import static org.junit.Assert.*;
-import org.junit.Assert;
+
+import java.util.Arrays;
 import org.junit.Test;
+
 public class testSelectionSort {
-@Test
-public void test() {
-testPositive();
-testNegative();
-testMixed();
-testDuplicates();
-}
-    public testSelectionSort() {
-    }
-    
+	@Test
+	public void test() {
+		testPositive();
+		testNegative();
+	}
+	SelectionSort s = new SelectionSort();
+
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -26,29 +23,38 @@ testDuplicates();
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
-        /** add tests to check for this unit test **/
+
+        int[] result = s.basicSelectionSort(arr);
+        assert Arrays.equals(result, Sortedarr);
+
     }
-    
+
     public void testNegative(){
-        
-        /** Test data contains negative values only **/
+
+        int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -7;
+        Sortedarr[4] = -2;
+
+        int[] result = s.basicSelectionSort(arr);
+        assert Arrays.equals(result, Sortedarr);
+
     }
-    
-    public void testMixed(){
-        
-        /** Test data contains with both positive, negative and zeros **/
-    }
-    
-    public void testDuplicates(){
-        
-        /** Test data contains duplicates **/
-    }
+
 }
